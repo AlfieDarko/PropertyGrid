3d33345386101cd8f69597781e4e60c1fef49f5d
AlfieDarko <alfie.darko1@tesco.com>
2019-11-07 17:48:30 +0000

    added section 4

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import React from 'react';
-import Header from './layout/Header'
-import Section1 from './layout/Section1';
-import Section2 from './layout/Section2';
-import Section3 from './layout/Section3'
+import React from "react";
+import Header from "./layout/Header";
+import Section1 from "./layout/Section1";
+import Section2 from "./layout/Section2";
+import Section3 from "./layout/Section3";
+import Section4 from "./layout/Section4";
+
 function App() {
   return (
-
     <div className="App">
-      <Header/>
-      <Section1/>
-      <Section2/>
-      <Section3/>
+      <Header />
+      <Section1 />
+      <Section2 />
+      <Section3 />
+      <Section4 />
     </div>
   );
 }
--- src/layout/Section3.js
+++ src/layout/Section3.js
@@ -7,7 +7,7 @@ const Section3 = () => {
   return (
     <div className="container-grid white-bg padding-top-10">
       <div className="section3__top-header--left">
-        <p className="xxxl-size">How it works?</p>
+        <p cntllassName="xxxl-size">How it works?</p>
       </div>
       <div className="section3__top-header--right md-size">
         <p>
@@ -15,33 +15,76 @@ const Section3 = () => {
           our lives. Many of us have at least been tempted at the flashy...
         </p>
       </div>
-
-      <div className="section3__mid-section-1--img-container">
-        <img src={img1} alt="" />
-      </div>
-
-      <div className="section3__mid-section-1--text-container">
-        <div className="flex-conainer">
-          <div className="grid-container">
-            <div className="div-number">01</div>
-            <h3 className="mid-section--h3">Brainstorming with Us</h3>
-            <p className="p__ white-text md-size md-lh">
-              Philosphy is considered a science but it is difficult to say when
-              one has to compare with an ordinary science, for example biology,
-              or chemistry. This is a question that turns into a burning problem
-              among the scientists and liguists all ove the world...
-            </p>
+      <div className="section3__mid-section-1">
+        <div className="section3__mid-section-1--img-container">
+          <img src={img1} alt="" />
+        </div>
+        <div className="section3__mid-section-1--text-container">
+          <div className="flex-conainer">
+            <div className="grid-container">
+              <div className="div-number">01</div>
+              <h3 className="mid-section--h3">Brainstorming with Us</h3>
+              <p className="p__ white-text md-size md-lh">
+                Philosphy is considered a science but it is difficult to say
+                when one has to compare with an ordinary science, for example
+                biology, or chemistry. This is a question that turns into a
+                burning problem among the scientists and liguists all ove the
+                world...
+              </p>
+            </div>
           </div>
         </div>
       </div>
-      <div className="section3__mid-section-2--img-container">
-        <img src={img2} alt="" />
+
+      <div className="section3__mid-section-2">
+        <div className="section3__mid-section-2--img-container">
+          <img src={img2} alt="" />
+        </div>
+        <div className="section3__mid-section-2--text-container">
+          <div className="flex-container">
+            <div className="grid-container">
+              <div className="section3__mid-section-2--text-container__div-number">
+                02
+              </div>
+              <h3 className="section3__mid-section-2--text-container__mid-section--h3">
+                Set Timeline Project
+              </h3>
+              <p className="p__ white-text md-size md-lh">
+                Philosphy is considered a science but it is difficult to say
+                when one has to compare with an ordinary science, for example
+                biology, or chemistry. This is a question that turns into a
+                burning problem among the scientists and liguists all ove the
+                world...
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="section3__mid-section-2--text-container">s</div>
-      <div className="section3__mid-section-3--img-container">
-        <img src={img3} alt="" />
+
+      <div className="section3__mid-section-3">
+        <div className="section3__mid-section-3--img-container">
+          <img src={img3} alt="" />
+        </div>
+        <div className="section3__mid-section-3--text-container">
+          <div className="flex-container">
+            <div className="grid-container">
+              <div className="section3__mid-section-3--text-container__div-number">
+                02
+              </div>
+              <h3 className="section3__mid-section-3--text-container__mid-section--h3">
+                Execution and Dealing
+              </h3>
+              <p className="p__ white-text md-size md-lh">
+                Philosphy is considered a science but it is difficult to say
+                when one has to compare with an ordinary science, for example
+                biology, or chemistry. This is a question that turns into a
+                burning problem among the scientists and liguists all ove the
+                world...
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="section3__mid-section-3--text-container">s</div>
     </div>
   );
 };
